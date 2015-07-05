9d4107eef1aed34e66cbc995b1b361364f67fa1c
tedda42 <tedda42@gmail.com>
2015-07-05 14:08:05 +1000

    Add stdout message, makefile and ignore file

---
+++ Makefile
@@ -0,0 +1,2 @@
+psr: PaperScissorsRock.c
+	gcc -o psr PaperScissorsRock.c -I .
---
+++ PaperScissorsRock.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Do you choose rock, paper or scissors?\n");
+  return 0;
+}
